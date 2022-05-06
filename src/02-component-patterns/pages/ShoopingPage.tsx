4322f060f87c7ed9ac451eac1587aad1ccc159bf
Jesus <jespinozaf07@gmail.com>
2022-05-06 15:30:55 -0600

    State Initializer

--- src/02-component-patterns/pages/ShoopingPage.tsx
+++ src/02-component-patterns/pages/ShoopingPage.tsx
@@ -7,8 +7,6 @@ import {
 
 import { products } from '../data/products';
 
-import '../styles/custom-styles.css';
-
 const product = products[0];
 
 export const ShoopingPage = () => {
@@ -19,7 +17,6 @@ export const ShoopingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
           maxCount: 10,
@@ -27,20 +24,9 @@ export const ShoopingPage = () => {
       >
         {({ reset, isMaxCountReached, maxCount, increaseBy, count }) => (
           <>
-            <ProductImage className="custom-image" />
-            <ProductTitle className="text-white" />
-            <ProductButtons className="custom-buttons" />
-
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-
-            <span>
-              {count} - {maxCount}
-            </span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
